5a8221aff4772ea22f00ee06877e744cc5f77df9
Oladipo Osinubi <diposinubi@gmail.com>
2019-08-07 11:23:12 -0400

    creating alphabet buttons

--- main.js
+++ main.js
@@ -2,5 +2,18 @@ let words = ["arsenal", "aston-villa", "bournemouth", "brighton", "burnley", "ch
 "leicester-city", "liverpool", "manchester-city", "manchester-united", "norwich-city", 
 "sheffield-united", "southhampton", "tottenham-hotspur", "watford", "westham-united", "wolverhampton"]
 
-let alphabets = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+let letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
                   'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    console.log(words[6]);
+
+//create buttons for each letter
+function createLetters() {
+    let newLet = document.createElement('li');
+}
+
+function addAlphabets(event){
+    event.preventDefault();
+    //append li element ul
+    document.querySelector('ul').appendChild(createLetters());
+}
