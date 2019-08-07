30b80997a6f41bed0b83672d4deecde0681870c3
Oladipo Osinubi <diposinubi@gmail.com>
2019-08-07 12:30:31 -0400

    randomize words

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
-let words = ["arsenal", "aston-villa", "bournemouth", "brighton", "burnley", "chelsea", "crystal-palace", "everton", 
-"leicester-city", "liverpool", "manchester-city", "manchester-united", "norwich-city", 
-"sheffield-united", "southhampton", "tottenham-hotspur", "watford", "westham-united", "wolverhampton"]
 
-let letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                  'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+//create variables
+let words = ["arsenal", "aston-villa", "bournemouth", "brighton", "burnley", "chelsea", "crystal-palace", "everton",
+    "leicester-city", "liverpool", "manchester-city", "manchester-united", "norwich-city",
+    "sheffield-united", "southhampton", "tottenham-hotspur", "watford", "westham-united", "wolverhampton"];
 
-    console.log(words[6]);
+let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//create buttons for each letter
-function createLetters() {
-    let newLet = document.createElement('li');
-}
 
-function addAlphabets(event){
-    event.preventDefault();
-    //append li element ul
-    document.querySelector('ul').appendChild(createLetters());
-}
+// randomize words 
+let word = words[Math.floor(Math.random() * words.length)];
+
