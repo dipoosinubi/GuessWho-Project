233121b519cd908fa8f4fcf83ba0448efaca8319
Oladipo Osinubi <diposinubi@gmail.com>
2019-08-07 10:23:22 -0400

    added arrays

---
+++ main.js
@@ -0,0 +1,6 @@
+let words = ["arsenal", "aston-villa", "bournemouth", "brighton", "burnley", "chelsea", "crystal-palace", "everton", 
+"leicester-city", "liverpool", "manchester-city", "manchester-united", "norwich-city", 
+"sheffield-united", "southhampton", "tottenham-hotspur", "watford", "westham-united", "wolverhampton"]
+
+let alphabets = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                  'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
