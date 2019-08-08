c96a0926df3a057f1ecdc638391d3d7822e4b95d
Oladipo Osinubi <diposinubi@gmail.com>
2019-08-08 04:48:42 -0400

    trying li tags

--- main.js
+++ main.js
@@ -6,25 +6,26 @@ let words = ["arsenal", "aston-villa", "bournemouth", "brighton", "burnley", "ch
 
 // creates word letters to choose from.
 let alphabets = document.getElementById("buttons");
-    for (var i=65; i<91; i++) {
-      var letters = document.createElement("input");
-      letters.type = "button";
-      letters.value = String.fromCharCode(i);
-      alphabets.appendChild(letters);
-    }
+for (let i = 65; i < 91; i++) {
+    let letters = document.createElement("input");
+    letters.type = "button";
+    letters.value = String.fromCharCode(i);
+    alphabets.appendChild(letters);
+}
 
 // randomize words 
 let word = words[Math.floor(Math.random() * words.length)];
+
 
 console.log(word);
 
 
 // create '*' based on the length of the word
-let blanks = document.getElementById("blanks")
-let hiddenWord = [];
-for (let i = 0; i < word.length; i++) {
-    hiddenWord.push('*');
-}
+let blanks = document.getElementById("blanks");
 
-blanks.innerHTML = hiddenWord
+for (let i = 0; i < word.length; i++) {
+    let hiddenWord = document.createElement('li');
+    hiddenWord.innerHTML = "*";
+    blanks.appendChild(hiddenWord);
+}
 
